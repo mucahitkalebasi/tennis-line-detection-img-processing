ea0d2f098a092b88ff450b090b53f8b27c55b1c3
Mücahit Kalebaşı <mucahitkalebasi@gmail.com>
2023-06-02 15:38:27 +0300

    initial commit

---
+++ court_detector.py
@@ -0,0 +1,324 @@
+import numpy as np
+import cv2
+#from matplotlib.pyplot as plt
+from sympy.geometry import Line
+from itertools import combinations
+from tennis_court_model import TennisCourtModel
+import ast
+import math
+
+class CourtDetector:
+
+  def __init__(self):
+    self.colour_threshold = 200
+    self.dist_tau = 3
+    self.intensity_threshold = 40
+    self.tennis_court_model = TennisCourtModel()
+    self.v_width = 0
+    self.v_height = 0
+    self.frame = None
+    self.gray = None
+    self.court_warp_matrix = []
+    self.game_warp_matrix = []
+
+
+  
+  def court_detect_from_image(self, img):
+    tuple_lines = []
+    lines = self.detect(img) # send the img array
+
+    for i in range(0, len(lines), 4):
+        first_point, second_point =  (int(lines[i]),int(lines[i+1])), (int(lines[i+2]), int(lines[i+3]))
+        tuple_lines.append(first_point)
+        tuple_lines.append(second_point)
+
+        cv2.line(img, first_point, second_point, (255,153,204), 2)
+        cv2.circle(img, first_point, 2, (0, 0, 255), -1)  # Circle for point 1
+        cv2.circle(img, second_point, 2, (0, 0, 255), -1)  # Circle for point 2
+
+ 
+    return tuple_lines, img
+  
+
+  def string_to_tuple(self, s):
+    # Convert a string-formatted tuple to an actual tuple
+    return tuple(map(int, s.strip('()').split(',')))
+  
+  def calculate_distances(self, truth_df, pred_df, image, tol):
+    tp, fp, fn = 0, 0, 0
+    for _, truth_row in truth_df.iterrows():
+        for _, pred_row in pred_df.iterrows():
+            if truth_row['point_name'] == pred_row['point_name']:
+                #print(f"Truth: {truth_row[image]}, Pred: {pred_row[image]}")  # Add this line
+                
+                a = ast.literal_eval(truth_row[image])
+                b = ast.literal_eval(pred_row[image])
+
+                dx = b[0] - a[0]
+                dy = b[1] - a[1]
+                distance = math.sqrt(dx*dx + dy*dy)
+
+                if distance <= tol:
+                    tp += 1
+                else:
+                    fp += 1
+    fn = len(truth_df) - tp
+    return tp, fp, fn
+  
+  def calculate_scores(self, truth_df, pred_df, tol):
+    images = [col for col in truth_df.columns if col not in ['point_name']]
+    results = {}
+    for image in images:
+        tp, fp, fn = self.calculate_distances(truth_df, pred_df, image, tol)
+        print("tp: ", tp, "fp: ",fp, "fn: ", fn)
+        precision = tp / (tp + fp) if (tp + fp) > 0 else 0
+        recall = tp / (tp + fn) if (tp + fn) > 0 else 0
+        f1 = 2 * precision * recall / (precision + recall) if (precision + recall) > 0 else 0
+        results[image] = {"Precision": precision, "Recall": recall, "F1-score": f1}
+    return results
+  
+  def detect(self, frame):
+    """
+    Detecting the court in the frame
+    """
+    self.frame = frame
+    self.v_height, self.v_width = frame.shape[:2]
+    # Get binary image from the frame
+    
+    self.gray = self._threshold(frame)
+    
+    # Filter pixel using the court known structure
+    filtered = self._filter_pixels(self.gray)
+    
+    # Detect lines using Hough transform
+    horizontal_lines, vertical_lines = self._detect_lines(filtered)
+    
+    # Find transformation from reference court to frame`s court
+    court_warp_matrix, game_warp_matrix = self._find_homography(horizontal_lines, vertical_lines)
+
+    self.court_warp_matrix.append(court_warp_matrix)
+    self.game_warp_matrix.append(game_warp_matrix)
+    
+    # Find important lines location on frame
+    return self.find_lines_location()
+
+  def _threshold(self, frame):
+    """
+    Simple thresholding for white pixels
+    """
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)[1]
+    return gray
+
+  def _filter_pixels(self, gray):
+    """
+    Filter pixels by using the court line structure
+    """
+    for i in range(self.dist_tau, len(gray) - self.dist_tau):
+      for j in range(self.dist_tau, len(gray[0]) - self.dist_tau):
+        if gray[i, j] == 0:
+          continue
+        if (gray[i, j] - gray[i + self.dist_tau, j] > self.intensity_threshold and gray[i, j] - gray[i - self.dist_tau, j] > self.intensity_threshold):
+          continue
+        if (gray[i, j] - gray[i, j + self.dist_tau] > self.intensity_threshold and gray[i, j] - gray[i, j - self.dist_tau] > self.intensity_threshold):
+          continue
+        gray[i, j] = 0
+    return gray
+
+  def _detect_lines(self, gray):
+        """
+        Finds all line in frame using Hough transform
+        """
+        minLineLength = 100
+        maxLineGap = 20
+        # Detect all lines
+        lines = cv2.HoughLinesP(gray, 1, np.pi / 180, 80, minLineLength=minLineLength, maxLineGap=maxLineGap)
+        lines = np.squeeze(lines)
+ 
+         # Classify the lines using their slope
+        horizontal, vertical = self._classify_lines(lines)
+        
+        # Merge lines that belong to the same line on frame
+        horizontal, vertical = self._merge_lines(horizontal, vertical)
+        return horizontal, vertical
+
+  def _classify_lines(self, lines):
+        """
+        Classify line to vertical and horizontal lines
+        """
+        horizontal = []
+        vertical = []
+        highest_vertical_y = np.inf
+        lowest_vertical_y = 0
+        for line in lines:
+            x1, y1, x2, y2 = line
+            dx = abs(x1 - x2)
+            dy = abs(y1 - y2)
+            if dx > 2 * dy:
+                horizontal.append(line)
+            else:
+                vertical.append(line)
+                highest_vertical_y = min(highest_vertical_y, y1, y2)
+                lowest_vertical_y = max(lowest_vertical_y, y1, y2)
+
+        # Filter horizontal lines using vertical lines lowest and highest point
+        clean_horizontal = []
+        h = lowest_vertical_y - highest_vertical_y
+        lowest_vertical_y += h / 15
+        highest_vertical_y -= h * 2 / 15
+        for line in horizontal:
+            x1, y1, x2, y2 = line
+            if lowest_vertical_y > y1 > highest_vertical_y and lowest_vertical_y > y1 > highest_vertical_y:
+                clean_horizontal.append(line)
+        return clean_horizontal, vertical
+
+  
+  def _merge_lines(self, horizontal_lines, vertical_lines):
+        """
+        Merge lines that belongs to the same frame`s lines
+        """
+
+        # Merge horizontal lines
+        horizontal_lines = sorted(horizontal_lines, key=lambda item: item[0])
+        mask = [True] * len(horizontal_lines)
+        new_horizontal_lines = []
+        for i, line in enumerate(horizontal_lines):
+            if mask[i]:
+                for j, s_line in enumerate(horizontal_lines[i + 1:]):
+                    if mask[i + j + 1]:
+                        x1, y1, x2, y2 = line
+                        x3, y3, x4, y4 = s_line
+                        dy = abs(y3 - y2)
+                        if dy < 10:
+                            points = sorted([(x1, y1), (x2, y2), (x3, y3), (x4, y4)], key=lambda x: x[0])
+                            line = np.array([*points[0], *points[-1]])
+                            mask[i + j + 1] = False
+                new_horizontal_lines.append(line)
+
+        # Merge vertical lines
+        vertical_lines = sorted(vertical_lines, key=lambda item: item[1])
+        xl, yl, xr, yr = (0, self.v_height * 6 / 7, self.v_width, self.v_height * 6 / 7)
+        mask = [True] * len(vertical_lines)
+        new_vertical_lines = []
+        for i, line in enumerate(vertical_lines):
+            if mask[i]:
+                for j, s_line in enumerate(vertical_lines[i + 1:]):
+                    if mask[i + j + 1]:
+                        x1, y1, x2, y2 = line
+                        x3, y3, x4, y4 = s_line
+                        xi, yi = line_intersection(((x1, y1), (x2, y2)), ((xl, yl), (xr, yr)))
+                        xj, yj = line_intersection(((x3, y3), (x4, y4)), ((xl, yl), (xr, yr)))
+
+                        dx = abs(xi - xj)
+                        if dx < 10:
+                            points = sorted([(x1, y1), (x2, y2), (x3, y3), (x4, y4)], key=lambda x: x[1])
+                            line = np.array([*points[0], *points[-1]])
+                            mask[i + j + 1] = False
+
+                new_vertical_lines.append(line)
+        return new_horizontal_lines, new_vertical_lines
+
+  def _find_homography(self, horizontal_lines, vertical_lines):
+        """
+        Finds transformation from reference court to frame`s court using 4 pairs of matching points
+        """
+        max_score = -np.inf
+        max_mat = None
+        max_inv_mat = None
+        # Loop over every pair of horizontal lines and every pair of vertical lines
+        for horizontal_pair in list(combinations(horizontal_lines, 2)):
+            for vertical_pair in list(combinations(vertical_lines, 2)):
+                h1, h2 = horizontal_pair
+                v1, v2 = vertical_pair
+                # Finding intersection points of all lines
+                i1 = line_intersection((tuple(h1[:2]), tuple(h1[2:])), (tuple(v1[0:2]), tuple(v1[2:])))
+                i2 = line_intersection((tuple(h1[:2]), tuple(h1[2:])), (tuple(v2[0:2]), tuple(v2[2:])))
+                i3 = line_intersection((tuple(h2[:2]), tuple(h2[2:])), (tuple(v1[0:2]), tuple(v1[2:])))
+                i4 = line_intersection((tuple(h2[:2]), tuple(h2[2:])), (tuple(v2[0:2]), tuple(v2[2:])))
+
+                intersections = [i1, i2, i3, i4]
+                intersections = sort_intersection_points(intersections)
+
+                for i, configuration in self.tennis_court_model.prepare_court_configs().items():
+                    # Find transformation
+                    matrix, _ = cv2.findHomography(np.float32(configuration), np.float32(intersections), method=0)
+                    inv_matrix = cv2.invert(matrix)[1]
+                    # Get transformation score
+                    confi_score = self._get_confi_score(matrix)
+
+                    if max_score < confi_score:
+                        max_score = confi_score
+                        max_mat = matrix
+                        max_inv_mat = inv_matrix
+                        self.best_conf = i
+   
+
+        return max_mat, max_inv_mat
+
+  def _get_confi_score(self, matrix):
+        """
+        Calculate transformation score
+        """
+        court = cv2.warpPerspective(self.tennis_court_model.court_img, matrix, self.frame.shape[1::-1])
+        court[court > 0] = 1
+        gray = self.gray.copy()
+        gray[gray > 0] = 1
+        correct = court * gray
+        wrong = court - correct
+        c_p = np.sum(correct)
+        w_p = np.sum(wrong)
+        return c_p - 0.5 * w_p
+
+
+  def add_court_overlay(self, frame, homography=None, overlay_color=(255, 255, 255), frame_num=-1):
+        """
+        Add overlay of the court to the frame
+        """
+        if homography is None and len(self.court_warp_matrix) > 0 and frame_num < len(self.court_warp_matrix):
+            homography = self.court_warp_matrix[frame_num]
+        court = cv2.warpPerspective(self.tennis_court_model.court_img, homography, frame.shape[1::-1])
+        frame[court > 0, :] = overlay_color
+        return frame
+
+  def find_lines_location(self):
+        """
+        Finds important lines location on frame
+        """
+        self.p = np.array(self.tennis_court_model.get_court_lines(), dtype=np.float32).reshape((-1, 1, 2))
+        self.lines = cv2.perspectiveTransform(self.p, self.court_warp_matrix[-1]).reshape(-1)
+
+        
+        return self.lines
+
+  def get_warped_court(self):
+        """
+        Returns warped court using the reference court and the transformation of the court
+        """
+        court = cv2.warpPerspective(self.tennis_court_model.court_img, self.court_warp_matrix[-1], self.frame.shape[1::-1])
+        court[court > 0] = 1
+        return court
+
+
+
+def line_intersection(line1, line2):
+    """
+    Find 2 lines intersection point
+    """
+    l1 = Line(line1[0], line1[1])
+    l2 = Line(line2[0], line2[1])
+    
+    intersection = l1.intersection(l2)
+    
+    
+    return intersection[0].coordinates
+
+def sort_intersection_points(intersections):
+    """
+    sort intersection points from top left to bottom right
+    """
+    y_sorted = sorted(intersections, key=lambda x: x[1])
+    p12 = y_sorted[:2]
+    p34 = y_sorted[2:]
+    p12 = sorted(p12, key=lambda x: x[0])
+    p34 = sorted(p34, key=lambda x: x[0])
+    return p12 + p34
---
+++ tennis_court_model.py
@@ -0,0 +1,226 @@
+import cv2
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+class TennisCourtModel:
+    """
+    Tennis Court reference model
+    """
+    def __init__(self):
+        # initialising points for image configuration
+        self.top_line = ((286, 561), (1379, 561))
+        self.bottom_line = ((286, 2935), (1379, 2935))
+        self.center_net = ((286, 1748), (1379, 1748))
+        self.left_court_border = ((286, 561), (286, 2935))
+        self.right_court_border = ((1379, 561), (1379, 2935))
+        self.left_inner_border = ((423, 561), (423, 2935))
+        self.right_inner_border = ((1242, 561), (1242, 2935))
+        self.center_line = ((832, 1110), (832, 2386))
+        self.top_inner_border = ((423, 1110), (1242, 1110))
+        self.bottom_inner_border = ((423, 2386), (1242, 2386))
+        self.top_extension = (832.5, 580)
+        self.bottom_extension = (832.5, 2910)
+
+        # Preparing court configurations
+        self.prepare_court_configs()
+
+        self.line_thickness = 1
+        self.court_w = 1117
+        self.court_h = 2408
+        self.top_bottom_padding = 549
+        self.right_left_padding = 274
+        self.total_court_w = self.court_w + self.right_left_padding * 2
+        self.total_court_h = self.court_h + self.top_bottom_padding * 2
+
+        self.points = ["top_left", "top_right", "bottom_left", "bottom_right", "middle_left", "middle_right", "top_left", 
+        "bottom_left", "top_right", "bottom_right", "left_middle_top", "left_middle_bottom", "right_middle_top", 
+        "right_middle_bottom", "middle_top", "middle_bottom", "second_middle_left", "second_middle_right", "middle_bottom_left", 
+        "middle_bottom_right"]
+
+        self.raw_dataframe_points = ["top_left",
+                                    "top_right",
+                                    "bottom_left",
+                                    "bottom_right",
+                                    "middle_left",
+                                    "middle_right",
+                                    "left_middle_top",  
+                                    "left_middle_bottom", 
+                                    "right_middle_top", 
+                                    "right_middle_bottom",
+                                    "middle_top",      
+                                    "middle_bottom",
+                                    "second_middle_left", 
+                                    "second_middle_right",
+                                    "middle_bottom_left", 
+                                    "middle_bottom_right"]
+
+        self.court_img = cv2.cvtColor(cv2.imread('court_configurations/court_reference.png'), cv2.COLOR_BGR2GRAY)
+
+    def prepare_court_configs(self):
+        self.court_configurations = {1: [*self.top_line, *self.bottom_line],
+                                     2: [self.left_inner_border[0], self.right_inner_border[0], 
+                                         self.left_inner_border[1], self.right_inner_border[1]],
+                                     3: [self.left_inner_border[0], self.right_court_border[0], 
+                                         self.left_inner_border[1], self.right_court_border[1]],
+                                     4: [self.left_court_border[0], self.right_inner_border[0], 
+                                         self.left_court_border[1], self.right_inner_border[1]],
+                                     5: [*self.top_inner_border, *self.bottom_inner_border],
+                                     6: [*self.top_inner_border, self.left_inner_border[1], 
+                                         self.right_inner_border[1]],
+                                     7: [self.left_inner_border[0], self.right_inner_border[0], 
+                                         *self.bottom_inner_border],
+                                     8: [self.right_inner_border[0], self.right_court_border[0], 
+                                         self.right_inner_border[1], self.right_court_border[1]],
+                                     9: [self.left_court_border[0], self.left_inner_border[0], 
+                                         self.left_court_border[1], self.left_inner_border[1]],
+                                     10: [self.top_inner_border[0], self.center_line[0], 
+                                          self.bottom_inner_border[0], self.center_line[1]],
+                                     11: [self.center_line[0], self.top_inner_border[1], 
+                                          self.center_line[1], self.bottom_inner_border[1]],
+                                     12: [*self.bottom_inner_border, self.left_inner_border[1], 
+                                          self.right_inner_border[1]]}
+        
+
+        return self.court_configurations
+    
+
+    def get_ground_truth_dataset(self):
+        ground_truth_pixels = {
+
+            "top_left" :            [(161,82), (596,237), (145,235), (337,168), (288,142)],
+            "top_right" :           [(450,82), (1373,239), (428,105), (1082,170), (1069,132)],
+            "bottom_left" :         [(70,323), (276,788), (896,563), (23,697), (30,707)],
+            "bottom_right" :        [(542,323), (1689,798), (1054,271), (1379,702), (1451,693)],
+            "middle_left" :         [(126,173), (484,432), (425,362), (228,354), (197,342)],
+            "middle_right" :        [(485,173), (1482,435), (688,172), (1184,355), (1201,330)],
+            "left_middle_top" :     [(198,82), (693,238), (188,215), (431,168), (386,142)],
+            "left_middle_bottom" :  [(128,324), (455,789), (924,512), (196,697), (211,704)],
+            "right_middle_top" :    [(414,82), (1275,240), (400,118), (988,170), (972,137)],
+            "right_middle_bottom" : [(483,323), (1506,796), (1041,296), (1206,700), (1268,694)],
+            "middle_top" :          [(306,119), (982,318), (416,198), (708,244), (688,220)],
+            "middle_bottom" :       [(306,244), (980,596), (777,318), (704,512), (716,502)],
+            "second_middle_left" :  [(187,119), (660,316), (301,261), (398,243), (362,222)],
+            "second_middle_right" : [(425,119), (1306,318), (508,148), (1018,245), (1013,216)],
+            "middle_bottom_left" :  [(151,244), (539,594), (683,417), (280,510), (272,506)],
+            "middle_bottom_right" : [(461,244), (1423,599), (849,242), (1128,513), (1163,498)]
+        }
+        ground_truth_dataset = pd.DataFrame(ground_truth_pixels)
+        ground_truth_dataset = self.preprocess_dataset(ground_truth_dataset)
+        
+        return ground_truth_dataset
+    
+
+    def preprocess_dataset(self, dataset, transpose = True):
+        # if ground truth icin
+        if transpose:
+            dataset = dataset.T
+            dataset = dataset.reset_index()
+            dataset.rename(columns = {'index':'point_name'}, inplace = True)
+            dataset.rename(columns={0: "image_0", 1: "image_1",
+                                    2: "image_2", 3: "image_3", 
+                                    4: "image_4"}, inplace=True)
+
+        else: 
+            dataset = dataset.reset_index()
+            dataset.rename(columns={0: "point_name"}, inplace=True)
+            dataset["point_name"].replace(to_replace=dataset["point_name"].values,
+                                           value=self.raw_dataframe_points,
+                                           inplace=True)
+
+
+        return dataset
+
+
+
+    def get_raw_prediction_dataset(self):
+        dictionary = {
+            "top_left",           
+            "top_right",         
+            "bottom_left",        
+            "bottom_right",       
+            "middle_left",        
+            "middle_right",     
+            "left_middle_top",  
+            "left_middle_bottom", 
+            "right_middle_top", 
+            "right_middle_bottom",
+            "middle_top",       
+            "middle_bottom",  
+            "second_middle_left", 
+            "second_middle_right",
+            "middle_bottom_left", 
+            "middle_bottom_right"
+        }
+
+        dataset = pd.DataFrame.from_dict(dictionary)
+        dataset = dataset.set_index([0])
+
+        return dataset
+        
+
+
+    def construct_court_model(self):
+        # Creating the court reference image based on the line positions
+        court_img = np.zeros((self.court_h + 2 * self.top_bottom_padding, 
+                              self.court_w + 2 * self.right_left_padding), dtype=np.uint8)
+        self.draw_lines(court_img)
+        court_img = cv2.dilate(court_img, np.ones((5, 5), dtype=np.uint8))
+        plt.imsave('court_configurations/court_reference.png', court_img, cmap='gray')
+        self.court_img = court_img
+        return court_img
+
+    def draw_lines(self, img):
+        cv2.line(img, *self.top_line, 1, self.line_thickness)
+        cv2.line(img, *self.bottom_line, 1, self.line_thickness)
+        cv2.line(img, *self.top_inner_border, 1, self.line_thickness)
+        cv2.line(img, *self.bottom_inner_border, 1, self.line_thickness)
+        cv2.line(img, *self.left_court_border, 1, self.line_thickness)
+        cv2.line(img, *self.right_court_border, 1, self.line_thickness)
+        cv2.line(img, *self.left_inner_border, 1, self.line_thickness)
+        cv2.line(img, *self.right_inner_border, 1, self.line_thickness)
+        cv2.line(img, *self.center_line, 1, self.line_thickness)
+
+    def get_court_lines(self):
+        """
+        Returns all lines of the court
+        """
+        all_lines = [*self.top_line, *self.bottom_line, *self.center_net, *self.left_court_border, 
+                     *self.right_court_border, *self.left_inner_border, *self.right_inner_border, 
+                     *self.center_line, *self.top_inner_border, *self.bottom_inner_border]
+        return all_lines
+
+    def get_extension_parts(self):
+        return [self.top_extension, self.bottom_extension]
+
+    def save_court_configs(self):
+        """
+        Create all configurations of 4 points on court reference
+        """
+        for i, config in self.court_configurations.items():
+            img = cv2.cvtColor(255 - self.court_img, cv2.COLOR_GRAY2BGR)
+            for point in config:
+                img = cv2.circle(img, point, 15, (0, 0, 255), 30)
+            cv2.imwrite(f'court_configurations/court_conf_{i}.png', img)
+
+    def get_court_mask(self, mask_type=0):
+        """
+        Get mask of the court
+        """
+        mask = np.ones_like(self.court_img)
+        if mask_type == 1:  # Bottom half court
+            mask[:self.center_net[0][1] - 1000, :] = 0
+        elif mask_type == 2:  # Top half court
+            mask[self.center_net[0][1]:, :] = 0
+        elif mask_type == 3: # court without margins
+            mask[:self.top_line[0][1], :] = 0
+            mask[self.bottom_line[0][1]:, :] = 0
+            mask[:, :self.left_court_border[0][0]] = 0
+            mask[:, self.right_court_border[0][0]:] = 0
+        return mask
+
+
+if __name__ == '__main__':
+    tennis_court = TennisCourtModel()
+    tennis_court.construct_court_model()
